a3a179419c34875a6a039d8c43593ac7188b6fde
csmith468 <chapin2816@gmail.com>
2025-11-02 19:07:44 -0600

    Azure deployemnt fixes: Node 20, code splitting, unused imports removed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'query-vendor': ['@tanstack/react-query'],
+          'form-vendor': ['react-hook-form'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
   server: { // temp
       proxy: {
         '/images': {
